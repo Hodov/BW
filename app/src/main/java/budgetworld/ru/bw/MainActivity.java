f3f03b6ab7ec46a00221d1b374a23f502557ff08
Hodov <ihodov@gmail.com>
2016-01-11 16:39:36 +0300

    New activity

--- app/src/main/java/budgetworld/ru/bw/MainActivity.java
+++ app/src/main/java/budgetworld/ru/bw/MainActivity.java
@@ -1,7 +1,10 @@
 package budgetworld.ru.bw;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -18,7 +21,7 @@ public class MainActivity extends Activity {
        // bwRest.getRestClient(2);
 
         ListView lvItems = (ListView) findViewById(R.id.lvItems);
-
+        //слушаем конец списка
         lvItems.setOnScrollListener(new EndlessScrollListener() {
             @Override
             public boolean onLoadMore(int page, int totalItemsCount) {
@@ -30,6 +33,20 @@ public class MainActivity extends Activity {
                 return true; // ONLY if more data is actually being loaded; false otherwise.
             }
         });
+
+        //слушаем клик
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                intent.putExtra("position", position);
+                intent.putExtra("title", bwRest.posts.get(position).postTitle);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 
 }
---
+++ app/src/main/java/budgetworld/ru/bw/PostActivity.java
@@ -0,0 +1,26 @@
+package budgetworld.ru.bw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by skorokhodov_a on 11.01.2016.
+ */
+public class PostActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_post);
+        Bundle extras = getIntent().getExtras();
+        System.out.println(extras.getInt("position"));
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Html.fromHtml(extras.getString("title")));
+
+    }
+}
