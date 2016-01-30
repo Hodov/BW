69b277037110328174ce5115d867c0cbb977217e
Hodov <ihodov@gmail.com>
2016-01-30 16:26:59 +0300

    Get fio and mobile number

--- app/src/main/java/budgetworld/ru/bw/MainActivity.java
+++ app/src/main/java/budgetworld/ru/bw/MainActivity.java
@@ -56,7 +56,10 @@ public class MainActivity extends AppCompatActivity {
         startGoogleAnalytics();
         startNotifications();
         startToolbar();
-        //SendSlackMessage slack = new SendSlackMessage("Вошел: "+getUsername());
+        User user = new User(this);
+        //SendSlackMessage slack = new SendSlackMessage("Событие: вход пользователя. Имя: " + user.userName + " Email: " + user.userEmail + " Моб.: " + user.userPhone);
+        System.out.println("Name: " + user.userName + " Email: " + user.userEmail + " Phone: " + user.userPhone);
+
 
         // если юзер пришел из нотификаций, пишем аналитику
         if (getIntent().hasExtra("from notify")) {
@@ -229,25 +232,6 @@ public class MainActivity extends AppCompatActivity {
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
 
-    public String getUsername() {
-        AccountManager manager = AccountManager.get(this);
-        Account[] accounts = manager.getAccountsByType("com.google");
-        List<String> possibleEmails = new LinkedList<String>();
 
-        for (Account account : accounts) {
-            // TODO: Check possibleEmail against an email regex or treat
-            // account.name as an email address only for certain account.type values.
-            possibleEmails.add(account.name);
-        }
-
-        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-            String email = possibleEmails.get(0);
-            String[] parts = email.split("@");
-
-            if (parts.length > 1)
-                return parts[0];
-        }
-        return null;
-    }
 
 }
---
+++ app/src/main/java/budgetworld/ru/bw/User.java
@@ -0,0 +1,40 @@
+package budgetworld.ru.bw;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Andrey on 30.01.2016.
+ */
+public class User {
+
+
+    public String userName = null;
+    public String userEmail = null;
+    public String userPhone = null;
+    private Context activity;
+
+    public User(Context activity) {
+        this.activity = activity;
+        getUser();
+    }
+
+    private void getUser() {
+        AccountManager manager = AccountManager.get(activity);
+
+        Account[] accountsVK = manager.getAccountsByType("com.vkontakte.account");
+        Account[] accountsViber = manager.getAccountsByType("com.viber.voip");
+        Account[] accountsGoogle = manager.getAccountsByType("com.google");
+
+        userName = accountsVK[0].name;
+        userEmail = accountsGoogle[0].name;
+        userPhone = accountsViber[0].name;
+
+        }
+
+}
